21d90a3250f7da1e13529a41aa4f36012321e9be
EnterWind11 <kaitoaokage@gmail.com>
2024-02-21 22:09:40 +0700

    Update

--- TryServer.cs
+++ TryServer.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Collections.Generic;
 
 namespace ServerSide
 {
@@ -10,27 +11,41 @@ namespace ServerSide
         static void Main(string[] args)
         {
             Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPEndPoint ipEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7029);
-            listenerSocket.Bind(ipEnd);
-            listenerSocket.Listen(int.MaxValue);
-            Console.WriteLine($"Waiting for a connection....");
+            IPAddress ipaddr = IPAddress.Any;
+            IPEndPoint ipEnd = new IPEndPoint(ipaddr, 7029);
             
-            Socket clientSocket = listenerSocket.Accept();
-            byte[] buffer = new byte[clientSocket.SendBufferSize];
-            Console.WriteLine($"Client Connected");
-            
-            int readByte;
-            do
+            try
             {
-                readByte = clientSocket.Receive(buffer);
-                byte[] recvData = new byte[readByte];
-                Array.Copy(buffer, recvData, readByte);
-                Console.WriteLine($"Received Data: " + recvData.ToString());
-            } while (readByte > 0);
-            
-            Console.WriteLine($"Client Disconnected");
-            Console.ReadKey();
+                listenerSocket.Bind(ipEnd);
+                listenerSocket.Listen(int.MaxValue);
+                Console.WriteLine($"Waiting for a connection....");
 
+                Socket clientSocket = listenerSocket.Accept();
+                Console.WriteLine($"Client Connected. " + clientSocket.ToString() + " - IP End Point: " + clientSocket.RemoteEndPoint.ToString());
+
+                List<byte> receivedData = new List<byte>();
+                byte[] buffer = new byte[clientSocket.ReceiveBufferSize];
+
+                int readBytes;
+                do
+                {
+                    readBytes = clientSocket.Receive(buffer);
+                    Console.WriteLine($"Number of received bytes: {readBytes}");
+                    receivedData.AddRange(buffer.Take(readBytes));
+                } while (readBytes > 0);
+
+                Console.WriteLine($"Received Data: " + Encoding.UTF8.GetString(receivedData.ToArray()));
+                Console.WriteLine($"Client Disconnected");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            finally
+            {
+                listenerSocket.Close();
+                Console.ReadKey();
+            }
         }
     }
 }
